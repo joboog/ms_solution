69b7d90a912bb021652cfdfda7ac0a07375b458d
joboog <johannesboog@yahoo.de>
2024-09-22 13:39:02 +0200

    [database] define pydantic models corresponding to db schemas

---
+++ database/pydantic_models.py
@@ -0,0 +1,61 @@
+from pydantic import BaseModel
+
+
+class CompoundBase(BaseModel):
+    compound_name: str
+    molecular_formula: str
+    type: str | None = None
+
+class CompoundCreate(CompoundBase):
+    compound_id: int
+
+class Compound(CompoundBase):
+    compound_id: int
+    #computed_mass: float
+
+    class Config:
+        orm_mode = True
+        
+
+class AdductBase(BaseModel):
+    adduct_name: str
+    mass_adjustment: float
+    ion_mode: str
+
+class AdductCreate(AdductBase):
+    pass
+
+class Adduct(AdductBase):
+    adduct_id: int
+
+    class Config:
+        orm_mode = True
+        
+
+class MeasuredCompoundBase(BaseModel):
+    compound_id: int
+    retention_time_id: int
+    adduct_id: int
+
+class MeasuredCompoundCreate(MeasuredCompoundBase):
+    pass
+
+class MeasuredCompound(MeasuredCompoundBase):
+    measured_compound_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class RetentionTimeBase(BaseModel):
+    retention_time: float
+    comment: str | None = None
+
+class RetentionTimeCreate(RetentionTimeBase):
+    pass
+
+class RetentionTime(RetentionTimeBase):
+    retention_time_id: int
+
+    class Config:
+        orm_mode = True
