cbbc72abf517c3818386f08e9e9547abf5c091a7
joboog <johannesboog@yahoo.de>
2024-09-24 16:05:40 +0200

    [database] remove unused io methods

--- database/io.py
+++ database/io.py
@@ -4,19 +4,6 @@ from . import pydantic_models, schema
 import pandas as pd
 
 
-def create_compound(db: Session, compound: pydantic_models.CompoundCreate):
-    db_compound = schema.Compound(
-        compound_id=compound.compound_id,
-        compound_name=compound.compound_name, 
-        molecular_formula=compound.molecular_formula,
-        type=compound.type
-    )
-    db.add(db_compound)
-    db.commit()
-    db.refresh(db_compound)
-    return db_compound
-
-
 def create_compounds(db: Session, compounds: list[pydantic_models.CompoundCreate]):
     db_compounds = [
         schema.Compound(
@@ -45,12 +32,6 @@ def get_compound_by_compound_name(db: Session, compound_name: str):
     )
     return result
 
-def get_compounds_by_ids(db: Session, compound_ids: list[int]):
-    result = (db.query(schema.Compound)
-                .filter(schema.Compound.compound_id.in_(compound_ids))
-                .all()
-    )
-    return result
 
 def get_compound_by_id_name(
     db: Session,
@@ -236,17 +217,6 @@ def get_retention_time_by_value_comment(
                 .filter(schema.RetentionTime.retention_time == retention_time,
                         schema.RetentionTime.comment == comment
                 )
-                .first()
-    )
-    return result
-
-
-def get_retention_time_by_value(
-    db: Session, 
-    retention_time: float
-    ):
-    result = (db.query(schema.RetentionTime)
-                .filter(schema.RetentionTime.retention_time == retention_time)
                 .first()
     )
     return result
