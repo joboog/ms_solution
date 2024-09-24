457daac6dc1874dda9a830948160a2b4e026c671
joboog <johannesboog@yahoo.de>
2024-09-25 01:38:54 +0200

    [doc] add chemical methods

--- database/chem.py
+++ database/chem.py
@@ -2,6 +2,16 @@ import re
 from pyteomics import mass
 
 def parse_formula(formula):
+    """
+    Parses a chemical formula and returns a standardized version of it.
+    This function takes a chemical formula as input and parses it to identify
+    isotopes, elements, and their counts. It then constructs a new formula
+    string in a standardized format.
+    Args:
+        formula (str): The chemical formula to be parsed.
+    Returns:
+        str: The standardized chemical formula.
+    """
 
     pattern = re.compile(r'(\[\d+\])?([A-Z][a-z]*)(\d*)')
     
@@ -23,6 +33,13 @@ def parse_formula(formula):
   
 
 def parse_and_compute_mass(formula):
+    """
+    Parses a chemical formula and computes its monoisotopic mass.
+    Args:
+        formula (str): The chemical formula to be parsed and computed.
+    Returns:
+        float: The monoisotopic mass of the given chemical formula.
+    """
     parsed_formula = parse_formula(formula)
     monoisotopic_mass = mass.calculate_mass(formula=parsed_formula)
     
@@ -31,6 +48,17 @@ def parse_and_compute_mass(formula):
    
 
 def update_molecular_formula(formula, adduct_name):
+    """
+    Updates the molecular formula based on the given adduct name.
+    Args:
+        formula (str): The original molecular formula.
+        adduct_name (str): The adduct name to modify the formula. 
+        Supported values are "M+H", "M-H", and "M+Na".
+    Returns:
+        str: The updated molecular formula.
+    Raises:
+        ValueError: If the formula contains invalid elements or counts.
+    """
     
     pattern = re.compile(r'([A-Z][a-z]*)(\d*)')
     
